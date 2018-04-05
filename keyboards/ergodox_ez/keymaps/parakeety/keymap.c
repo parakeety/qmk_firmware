513a7badf6b521c4b83120ff0dd8605f69f486df
yohei okada <okada.yohei@gmail.com>
2018-04-05 10:38:45 +0900

    Assign MINS to RSFT & Alt

--- keyboards/ergodox_ez/keymaps/parakeety/keymap.c
+++ keyboards/ergodox_ez/keymaps/parakeety/keymap.c
@@ -19,9 +19,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  Ent   |
  * |--------+------+------+------+------+------|  '   |           |   ]  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |  -     |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LGUI |  '"  |AltShf| Left | Right|                                       |  Up  | Down | Left | Right| ~L1  |
+ *   | LGUI |  '"  |AltShf|  -   |  Alt |                                       |  Up  | Down | Left | Right| ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_EQL,
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_QUOT,
-        KC_LGUI, KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+        KC_LGUI, KC_QUOT,      LALT(KC_LSFT),  KC_MINS, KC_LALT,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_LGUI, LGUI(KC_LSFT), KC_HANJ,
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_MINS,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_DEL,
              KC_LBRC,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_ENT,
-             KC_RBRC, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,
+             KC_RBRC, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_MINS,
                                   KC_UP,  KC_DOWN,KC_LEFT,KC_RGHT,          KC_FN1,
              KC_LALT,        LGUI(KC_LALT),
              KC_PGUP,
