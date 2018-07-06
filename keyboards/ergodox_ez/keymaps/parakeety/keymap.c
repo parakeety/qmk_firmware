9cfece9cca5f0505e5bde636d77b3f864506d472
yohei okada <okada.yohei@gmail.com>
2018-07-06 10:43:03 +0900

    Left shift right numbers

--- keyboards/ergodox_ez/keymaps/parakeety/keymap.c
+++ keyboards/ergodox_ez/keymaps/parakeety/keymap.c
@@ -13,21 +13,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | ESC  |           |   -  |   6  |   7  |   8  |   9  |   0  |  DEL   |
+ * |  ~     |   1  |   2  |   3  |   4  |   5  |  ESC |           |   6  |   7  |   8  |   9  |   0  |   -  |   =    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  =   |           |   [  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  Ent   |
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   Ent  |
  * |--------+------+------+------+------+------|  '   |           |   ]  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |  -     |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   -    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LGUI |  '"  |AltShf|  -   |  Alt |                                       |  Up  | Down | Left | Right| ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        | App  | LGui |       | Alt  |  Del   |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | LGUI |LGUI+ |------|       |------|  Back  |Space |
+ *                                 | LGUI |LGUI+ |------|       |------|  Bsp   |Space |
  *                                 |      |Shift | Ja   |       | Eng  |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_GRAVE,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_ESC,
+        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_ESC,
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_EQL,
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_QUOT,
@@ -44,14 +44,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_HOME,
                                                KC_LGUI, LGUI(KC_LSFT), KC_HANJ,
         // right hand
-             KC_MINS,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_DEL,
+             KC_6,   KC_7,   KC_8,   KC_9,   KC_0,  KC_MINS, KC_EQL,
              KC_LBRC,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_ENT,
-             KC_RBRC, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_MINS,
+             KC_RBRC, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,
                                   KC_UP,  KC_DOWN,KC_LEFT,KC_RGHT,          KC_FN1,
-             KC_LALT,        LGUI(KC_LALT),
+             KC_LALT,        KC_DEL,
              KC_PGUP,
-             KC_HAEN,KC_BSPC, KC_SPC
+             KC_HAEN, KC_BSPC ,KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
