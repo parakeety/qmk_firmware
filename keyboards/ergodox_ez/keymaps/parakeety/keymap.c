0e1e63f72f4d9c78ca0b3b389b31499c4549a0d3
yohei okada <okada.yohei@gmail.com>
2018-12-27 16:54:19 +0900

    Add Colemak

--- keyboards/ergodox_ez/keymaps/parakeety/keymap.c
+++ keyboards/ergodox_ez/keymaps/parakeety/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_QUOT,
         KC_LGUI, KC_QUOT,      LALT(KC_LSFT),  KC_MINS, KC_LALT,
-                                              ALT_T(KC_APP),  KC_LGUI,
+                                              KC_TAB,  KC_LGUI,
                                                               KC_HOME,
                                                KC_LGUI, LGUI(KC_LSFT), KC_HANJ,
         // right hand
@@ -76,24 +76,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
-       // left hand
-       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       KC_TRNS,KC_TRNS,
-                                               KC_TRNS,
-                               KC_TRNS,KC_TRNS,KC_TRNS,
-       // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+        // left hand
+        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_ESC,
+        KC_TAB,        KC_Q,         KC_W,   KC_F,   KC_P,   KC_G,   KC_EQL,
+        KC_LCTL,        KC_A,         KC_R,   KC_S,   KC_T,   KC_D,
+        KC_LSFT,        KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_QUOT,
+        KC_LGUI, KC_QUOT,      LALT(KC_LSFT),  KC_MINS, KC_LALT,
+                                              KC_TAB,  KC_LGUI,
+                                                              KC_HOME,
+                                               KC_LGUI, LGUI(KC_LSFT), KC_HANJ,
+        // right hand
+             KC_6,   KC_7,   KC_8,   KC_9,   KC_0,  KC_MINS, KC_EQL,
+             KC_LBRC,       KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,             KC_BSLS,
+                          KC_H,   KC_N,   KC_E,   KC_I,   KC_O, KC_ENT,
+             KC_RBRC, KC_K,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,
+                                  KC_UP,  KC_DOWN,KC_LEFT,KC_RGHT,          KC_TRNS,
+             KC_LALT,        KC_DEL,
+             KC_PGUP,
+             KC_HAEN, KC_BSPC ,KC_SPC
 ),
 /* Keymap 2: Media and mouse keys
  *
